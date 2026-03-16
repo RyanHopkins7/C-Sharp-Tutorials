adb0abd49b4c8fd6066a7111dad68c5776b3489e
Ryan Hopkins <ryan@ryanhopkins.dev>
2026-03-16 11:16:01 +0900

    Add hello_world_simple.cs

---
+++ hello_world_simple.cs
@@ -0,0 +1,3 @@
+// Source: https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/overview
+// Same functionality as hello_world.cs but with less code
+System.Console.WriteLine("Hello, World");
