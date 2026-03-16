e17e86f40fbaf1ceb9a9e9b4e5d48c8791e92597
Ryan Hopkins <ryan@ryanhopkins.dev>
2026-03-16 10:42:41 +0900

    Add hello_world.cs

---
+++ hello_world.cs
@@ -0,0 +1,12 @@
+// Source: https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/overview
+using System;
+namespace TourOfCSharp;
+
+class Program
+{
+    static void Main()
+    {
+        // This line prints "Hello, World"
+        Console.WriteLine("Hello, World");
+    }
+}
