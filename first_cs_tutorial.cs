0a7a10cd2fd51c9cc9b28780e9bc6bc28579e630
Ryan Hopkins <ryan@ryanhopkins.dev>
2026-03-17 12:05:14 +0900

    Add challenge code to first_cs_tutorial.cs

--- first_cs_tutorial.cs
+++ first_cs_tutorial.cs
@@ -1,7 +1,12 @@
 // Source: https://learn.microsoft.com/en-us/training/modules/csharp-write-first/
 // Console.WriteLine("Hello, World!");
 
-// WriteLine() writes a string to console with a new line
+// WriteLine() writes a string to console with a new line at the end
 Console.WriteLine("Congragulations!");
 // Write() writes a string to console without a new line
 Console.Write("You just wrote your first lines of C# code.");
+
+Console.WriteLine();
+Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.");
+Console.WriteLine();
