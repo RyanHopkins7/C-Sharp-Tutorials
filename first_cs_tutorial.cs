639d6ed19c8ff6acefc95e6cc8d9d4e7e9b4822d
Ryan Hopkins <ryan@ryanhopkins.dev>
2026-03-17 11:55:45 +0900

    Add first_cs_tutorial.cs

---
+++ first_cs_tutorial.cs
@@ -0,0 +1,7 @@
+// Source: https://learn.microsoft.com/en-us/training/modules/csharp-write-first/
+// Console.WriteLine("Hello, World!");
+
+// WriteLine() writes a string to console with a new line
+Console.WriteLine("Congragulations!");
+// Write() writes a string to console without a new line
+Console.Write("You just wrote your first lines of C# code.");
